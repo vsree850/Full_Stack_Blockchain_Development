3f5103d87f85ff17e4e20bfd2d487f2b2db1763a
vsree850 <51875282+vsree850@users.noreply.github.com>
2022-11-14 14:04:09 +0530

    Add files via upload

--- JavaScript_Assignments/Assignment_5/Q_1.js
+++ JavaScript_Assignments/Assignment_5/Q_1.js
@@ -1,1 +1,43 @@
+class Shape {
+  constructor(radius, height) {
+    this.height = height;
+    this.radius = radius;
+  }
+}
 
+class Cylinder extends Shape {
+  constructor(radius, height) {
+    super(radius, height);
+  }
+  getVolume() {
+    return (Math.PI * this.radius ** 2 * this.height).toFixed(4);
+  }
+}
+
+class Sphere extends Shape {
+  constructor(radius) {
+    super(radius);
+  }
+  getVolume() {
+    return ((4 / 3) * Math.PI * this.radius ** 3).toFixed(4);
+  }
+}
+
+class Cone extends Shape {
+  constructor(radius, height) {
+    super(radius, height);
+  }
+  getVolume() {
+    let val = ((Math.PI * this.radius ** 2 * this.height) / 3).toFixed(4);
+    return val;
+  }
+}
+
+let obj1 = new Cylinder(5, 6);
+console.log(obj1.getVolume());
+
+let obj2 = new Sphere(5);
+console.log(obj2.getVolume());
+
+let obj3 = new Cone(5, 6);
+console.log(obj3.getVolume());
