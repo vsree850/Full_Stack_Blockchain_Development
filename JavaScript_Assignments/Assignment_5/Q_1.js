535fb5f32047f1ac1e31a8b44f7195c9689d2392
vsree850 <51875282+vsree850@users.noreply.github.com>
2022-11-14 14:06:34 +0530

    Add files via upload

--- JavaScript_Assignments/Assignment_5/Q_1.js
+++ JavaScript_Assignments/Assignment_5/Q_1.js
@@ -1,3 +1,18 @@
+// 1) Write a JavaScript program to get the volume of a Cylinder, Sphere and
+// Cone with four decimal places using objects and classes.
+// Create classes for volumes for each geometric shape which returns the
+// output using the getVolume() method.
+// eg- to get volume of cylinderlet
+// obj= new Cylinder(radius,height);
+// obj.getVolume();
+// Formulas for volumes of the shapes-
+// 1) Cylinder- Volume = πr2h
+// where r is the radius and h is the height of the cylinder.
+// 2)Sphere- Volume= 4/3π𝑟3
+// where r is the radius
+// 3) Cone- Volume= πr2h/3
+// where r is the radius and h is the height of the cone.
+
 class Shape {
   constructor(radius, height) {
     this.height = height;
